99d01f71293fbc41ec2b4bdd6eb97615e929adc3
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-06-20 10:06:57 -0500

    Create calc.rkt

---
+++ Racket/calc.rkt
@@ -0,0 +1,45 @@
+#lang racket
+(define MAX 999)
+(define MIN -999)
+(define (add f s)
+  (cond [(not (integer? f)) (error "Parsing Error")]
+        [(not (integer? s)) (error "Parsing Error")]
+        [(> (+ f s) MAX) (error "Overflow Error")]
+        [(< (+ f s) MIN) (error "Overflow Error")]
+        [else (+ f s)]))
+(define (sub f s)
+  (cond [(not (integer? f)) (error "Parsing Error")]
+        [(not (integer? s)) (error "Parsing Error")]
+        [(> (- f s) MAX) (error "Overflow Error")]
+        [(< (- f s) MIN) (error "Overflow Error")]
+        [else (- f s)]))
+(define (mul f s)
+  (cond [(not (integer? f)) (error "Parsing Error")]
+        [(not (integer? s)) (error "Parsing Error")]
+        [(> (* f s) MAX) (error "Overflow Error")]
+        [(< (* f s) MIN) (error "Overflow Error")]
+        [else (* f s)]))
+(define (div f s)
+  (cond [(not (integer? f)) (error "Parsing Error")]
+        [(not (integer? s)) (error "Parsing Error")]
+        [(> (/ f s) MAX) (error "Overflow Error")]
+        [(< (/ f s) MIN) (error "Overflow Error")]
+        [else (/ f s)]))
+(define (mod f s)
+  (cond [(not (integer? f)) (error "Parsing Error")]
+        [(not (integer? s)) (error "Parsing Error")]
+        [(> (remainder f s) MAX) (error "Overflow Error")]
+        [(< (remainder f s) MIN) (error "Overflow Error")]
+        [else (remainder f s)]))
+(define (exp f s)
+  (cond [(not (integer? f)) (error "Parsing Error")]
+        [(not (integer? s)) (error "Parsing Error")]
+        [(> (expt f s) MAX) (error "Overflow Error")]
+        [(< (expt f s) MIN) (error "Overflow Error")]
+        [else (expt f s)]))
+(define (srt f)
+  (cond [(not (integer? f)) (error "Parsing Error")]
+        [(> (sqrt f) MAX) (error "Overflow Error")]
+        [(< (sqrt f) MIN) (error "Overflow Error")]
+        [else (sqrt f)]))
+"Enter an equation."
