b1e1ab6e6cebe688d9c55f9a332d3df08b7d08c0
Michael Rutherford <michaellogan.rutherford@gmail.com>
2016-04-09 20:40:38 -0500

    Simplified kernel module

--- C/Kernel-Modules/hello-module.c
+++ C/Kernel-Modules/hello-module.c
@@ -1,26 +1,20 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michael Rutherford");
-MODULE_DESCRIPTION("A simple kernel module");
-
-static char* hello_message = "Hello, Kernel";
-
-module_param(hello_message, charp, 0000);
-MODULE_PARM_DESC(hello_message, "The string that holds the message.");
+MODULE_DESCRIPTION("A simple kernel module.");
 
 static int __init hello_init(void)
 {
-	printk(KERN_INFO "%s\n", hello_message);
-	return 0;
+        printk(KERN_INFO "Hello, Kernel!\n");
+        return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_INFO "Goodbye, Kernel!\n");
+        printk(KERN_INFO "Goodbye, Kernel!\n");
 }
 
 module_init(hello_init);
